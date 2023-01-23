1f4959a426e08e4f476068c8b38790fcf3874b06
devrajash <ashdevrajttx@gmail.com>
2023-01-23 20:33:30 +0530

    config remove

--- src/main/java/pringboot1st/springboot1/config/WebMvcConfig.java
+++ src/main/java/pringboot1st/springboot1/config/WebMvcConfig.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-   @Override
-   public void addCorsMappings(CorsRegistry registry) {
-       WebMvcConfigurer.super.addCorsMappings(registry);
-       registry.addMapping("/**").allowedOrigins("*");
-   }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 
 }
